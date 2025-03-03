8d93dc3f9cc217a40723165cc662548689ddc88b
Óscar López García <UO294515@uniovi.es>
2025-03-03 19:50:57 +0100

    s3

---
+++ algstudent/s3/Subtraction4.java
@@ -0,0 +1,34 @@
+package algstudent.s3;
+
+/* Class that models T(n)=T(n-1)+O(n)
+ * The time complexity is quadratic O(n^2) 
+ * and the waste of stack is O(n)
+ * In this case => the stack overflows 
+ */
+public class Subtraction4 {
+	public static long rec2(int n) {
+		int cont = 0;
+		if (n <= 0)
+			cont++;
+		else {
+			for (int i = 0; i < n; i++)
+				for(int j=0;j<n;j++)
+					cont++; // O(n)
+			rec2(n - 1);
+		}
+		return cont;
+	}
+
+	public static void main(String arg[]) {
+		long t1, t2, cont = 0;
+		for (int n = 100; n <= 100000; n *= 2) {
+			t1 = System.currentTimeMillis();
+
+			cont = rec2(n);
+
+			t2 = System.currentTimeMillis();
+
+			System.out.println("n=" + n + "**TIME=" + (t2 - t1) + "**cont=" + cont);
+		} // for
+	} // main
+} // class
